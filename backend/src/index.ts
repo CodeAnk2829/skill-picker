c493f1e03502431a50f2c84ddf1a66c00149ff4f
Ankit <211112004@stu.manit.ac.in>
2024-09-15 06:02:19 +0530

    add types to req and res

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,15 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/v1/get-skills", async (req, res) => {
+app.get("/api/v1/get-skills", async (req: any, res: any) => {
     try {
-        console.log(SKILLS);
         return res.status(200).json({
             ok: true,
             skills: SKILLS
         });
     } catch(err) {
-        console.log(err);
         return res.status(404).json({
             ok: false
         });
