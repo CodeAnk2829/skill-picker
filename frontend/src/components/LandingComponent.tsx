dcc24c0ea8c0394dd1b4153a03ec490b4f3c41f6
Ankit <211112004@stu.manit.ac.in>
2024-09-15 06:32:00 +0530

    feat(client): add router

---
+++ frontend/src/components/LandingComponent.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+
+export default function LandingComponent() {
+    const [skillList, setSkillList] = useState<string[]>([]);
+    const [skill, setSkill] = useState<string[]>([]);
+
+    useEffect(() => {
+        fetch(`https://skill-picker-3.onrender.com/api/v1/get-skills`)
+            .then(async (res) => {
+                const json = await res.json();
+                setSkillList(json.skills);
+            })
+    }, []);
+
+    async function handleClick(skill: string, key: number) {
+        const selectedSkill: string = skill;
+        setSkill(skill => [...skill, selectedSkill]);
+        setSkillList(skillList.filter((value, index) => index != key));
+        skillList.sort();
+    }
+
+    async function buttonHandler(key: number) {
+        // remove the selected skill from the skill sections
+        const removedSkill = skill[key];
+        setSkill(skill.filter((value, index) => { return index != key }));
+        setSkillList((s: string[]) => [...s, removedSkill]);
+        skillList.sort();
+    }
+
+    async function buttonHandlerToDeleteAll() {
+        setSkillList((s) => {
+            const newSKillSet = [...s];
+            skill.forEach((skillValue) => {
+                newSKillSet.push(skillValue);
+            });
+            return newSKillSet;
+        });
+        skillList.sort();
+        setSkill([]);
+    }
+
+    return (
+        <div className='flex flex-col items-center p-5 h-screen w-full justify-center'>
+            <div className='grid grid-cols-8 content-center h-16 w-1/3 bg-white mb-5 overflow-y-auto text-balance p-3 border-2 border-slate-300 rounded-sm'>
+                <div className='col-span-7'>
+                    {/* make another component */}
+                    {skill.map((value, key) => {
+                        return (
+                            <span className='bg-slate-200 h-5 p-1 text-xs font-medium text-slate-600 rounded-md mr-2' key={key}>
+                                <span className='mr-1 rounded-md'>{value}</span>
+                                <button className='
+                                    hover:bg-slate-200 
+                                    p-0.5 
+                                    rounded-sm'
+                                    onClick={() => {
+                                        buttonHandler(key)
+                                    }}
+                                >
+                                    <svg className="h-3 w-3 text-slate-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>                </button>
+                            </span>
+                        )
+                    })}
+                </div>
+                <div className='flex justify-end'>
+                    <button onClick={buttonHandlerToDeleteAll}>
+                        <svg className="h-5 w-5 text-slate-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg>
+                    </button>
+                </div>
+            </div>
+            <div className='h-60 w-1/3 bg-white overflow-y-auto scrollbar-hide border-2 border-slate-300 rounded-sm'>
+                <ul role='list'>
+                    {skillList.map((skill, key) => {
+                        return <li key={key} className='group/item  h-10 p-3  cursor-pointer content-center' onClick={() => handleClick(skill, key)}>
+                            <div className='hover:bg-violet-200 hover: rounded-md p-2 text-slate-500 font-medium'>{skill}</div>
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </div>
+    )
+}
